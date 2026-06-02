220288bf1a88da4264a76abc77b8849a2ac3d235
taisuiyileba <3416219326@qq.com>
2026-06-02 10:15:32 +0800

    Initial commit: MobSpawnSwitch mod with server-only support

---
+++ src/main/java/com/mobspawnswitch/Mobspawnswitch.java
@@ -0,0 +1,47 @@
+package com.mobspawnswitch;
+
+import com.mobspawnswitch.command.MobSpawnManager;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.event.server.ServerStoppingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.nio.file.Path;
+
+@Mod(Mobspawnswitch.MODID)
+public class Mobspawnswitch {
+
+    public static final String MODID = "mobspawnswitch";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Mobspawnswitch() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("MobSpawnSwitch initializing");
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        Path configDir = event.getServer().getWorldPath(net.minecraft.world.level.storage.LevelResource.ROOT)
+                .resolve("data").resolve(MODID);
+        MobSpawnManager.setSavePath(configDir.resolve("rules.json"));
+        MobSpawnManager.load();
+        LOGGER.info("MobSpawnSwitch loaded rules");
+    }
+
+    @SubscribeEvent
+    public void onServerStopping(ServerStoppingEvent event) {
+        MobSpawnManager.save();
+        LOGGER.info("MobSpawnSwitch saved rules");
+    }
+}
